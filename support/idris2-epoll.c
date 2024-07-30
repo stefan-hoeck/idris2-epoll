fb13aa3090043e2403ac75a222140f66bebae5db
stefan-hoeck <hock@zhaw.ch>
2024-07-30 21:51:56 +0200

    [ new ] ctl operations

--- support/idris2-epoll.c
+++ support/idris2-epoll.c
@@ -2,6 +2,26 @@
 #include <errno.h>
 #include <sys/epoll.h>
 
+//
+// Error codes
+//
+
 int ep_eagain() {
   return EAGAIN;
+}
+
+//
+// Operations
+//
+
+int ep_epoll_ctl_add() {
+  return EPOLL_CTL_ADD;
+}
+
+int ep_epoll_ctl_mod() {
+  return EPOLL_CTL_MOD;
+}
+
+int ep_epoll_ctl_del() {
+  return EPOLL_CTL_DEL;
 }
